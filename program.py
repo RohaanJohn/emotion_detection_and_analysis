a1b84468b6822d2244f00f827ce3a6ef54fcab1c
Noel John Robert <noel060202@gmail.com>
2023-12-04 15:13:05 +0530

    code improvement

--- program.py
+++ program.py
@@ -1,19 +1,38 @@
 import cv2
+import os
+import time
+import shutil
+
+
+photos_folder = 'temp_photos'
+if os.path.exists(photos_folder):
+    shutil.rmtree(photos_folder)
+os.mkdir(photos_folder)
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 cap = cv2.VideoCapture(0)
 
+counter = 0
 while True:
     ret, frame = cap.read()
     
-    if not ret: break
+    if not ret:
+        break
     
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
     
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        
+        cropped_face = frame[y:y+h, x:x+w]
+        
+        if counter % 40 == 0:
+            file_name = f'{photos_folder}/face_{int(time.time())}.jpg'
+            cv2.imwrite(file_name, cropped_face)
+    
+    counter += 1
         
     cv2.imshow('Face Detection', frame)
     
