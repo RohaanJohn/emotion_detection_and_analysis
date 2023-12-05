a4833456023eb553a8fb7e523088bdb46df4d709
Noel John Robert <noel060202@gmail.com>
2023-12-05 23:19:00 +0530

    bounding box functionality improvement

--- program.py
+++ program.py
@@ -3,6 +3,13 @@ import os
 import time
 import shutil
 
+#  placeholder function
+def detect(face_image):
+    return f"{int(time.time())}"
+
+
+
+
 
 photos_folder = 'temp_photos'
 if os.path.exists(photos_folder):
@@ -14,6 +21,8 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
 
 counter = 0
+time_var = 125   # increase this to increase time between shots
+detected_text = ""
 while True:
     ret, frame = cap.read()
     
@@ -24,11 +33,29 @@ while True:
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
     
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # Increase the bounding box size by multiplying w and h with a scaling factor
+        padding = 0.3  # Change this value to adjust the size of the bounding box
         
-        cropped_face = frame[y:y+h, x:x+w]
+        # Calculate new x, y, w, h with increased size
+        new_w = int(w * (1 + padding))
+        new_h = int(h * (1 + padding))
+        new_x = max(int(x - (padding * w / 2)), 0)
+        new_y = max(int(y - (padding * h / 2)), 0)
         
-        if counter % 40 == 0:
+        # bounding box
+        cv2.rectangle(frame, (new_x, new_y), (new_x + new_w, new_y + new_h), (0, 255, 0), 2)
+        
+        # Crop the frame according to the adjusted coordinates
+        # cropped_face = frame[new_y:new_y+new_h, new_x:new_x+new_w]
+        # detected_text = detect(cropped_face)
+        cv2.putText(frame, detected_text, (new_x, new_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        
+        # Save the cropped face after a time interval
+        if counter % time_var == 0:
+            cropped_face = frame[new_y:new_y+new_h, new_x:new_x+new_w]
+            detected_text = detect(cropped_face)
+            # cv2.putText(frame, detected_text, (new_x, new_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        
             file_name = f'{photos_folder}/face_{int(time.time())}.jpg'
             cv2.imwrite(file_name, cropped_face)
     
@@ -41,33 +68,3 @@ while True:
     
 cap.release()
 cv2.destroyAllWindows()
-
-
-
-
-
-
-
-
-
-
-# import cv2
-
-# # Load the pre-trained Haar Cascade face classifier
-# face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-# # Read the input image
-# image = cv2.imread('noel.jpg')
-# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# # Detect faces
-# faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
-# # Draw rectangles around detected faces
-# for (x, y, w, h) in faces:
-#     cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
-
-# # Display the result
-# cv2.imshow('Detected Faces', image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
